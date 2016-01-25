0b445acd2cd7e4beb99477e00776256b84516133
Tom Bredemeier <tbredemeier@barrington220.org>
2016-01-25 14:11:19 -0600

    Initial Commit

---
+++ College Profile BuilderTests/College_Profile_BuilderTests.swift
@@ -0,0 +1,36 @@
+//
+//  College_Profile_BuilderTests.swift
+//  College Profile BuilderTests
+//
+//  Created by tbredemeier on 1/25/16.
+//  Copyright © 2016 Zephyr Technology. All rights reserved.
+//
+
+import XCTest
+@testable import College_Profile_Builder
+
+class College_Profile_BuilderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
