414b712097309bee1a15d9eb2a18c3eaed6f8a33
Tom Bredemeier <tbredemeier@barrington220.org>
2016-01-25 14:30:04 -0600

    Connect Storyboard and added protocols

--- College Profile Builder/ViewController.swift
+++ College Profile Builder/ViewController.swift
@@ -8,18 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    var colleges = ["Texas Christian University", "University of Iowa", "Lake Forest GSM"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
